007baf0eca8742812c2ba0c43dbf15e3630d937d
Vadim Trufanov <vadim.trufanov@gotbit.io>
2023-06-27 15:15:42 +0300

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ethers } from "ethers";
+import MulticallWrapper from "ethers-multicall-provider/lib/multicall-provider";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed.binance.org/"
 );
-root.render(
+
+const wrapper = MulticallWrapper.wrap(provider);
+console.log(wrapper);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
